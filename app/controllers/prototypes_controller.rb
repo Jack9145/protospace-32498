b53651d3ff80b68ec36ee5240ddb706101122f8a
Jack9145 <toikakunie@gmail.com>
2020-12-02 18:39:19 +0900

    コメントへの再編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :edit, :update , :destroy]
   before_action :find_id , only: [:show, :edit] 
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update , :destroy]
   
   def index
     @prototypes = Prototype.all
